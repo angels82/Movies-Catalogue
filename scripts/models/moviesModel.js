ff954ee75f3e73784f6005d84552fd6e363fa996
bosak <bosakmaw@gmail.com>
2016-12-05 16:08:38 +0200

    create Movies model class

--- scripts/models/moviesModel.js
+++ scripts/models/moviesModel.js
@@ -1,15 +1,30 @@
-// GET
-// Get data from movies collection
-// and sends it to moviesController
+class MovieModel {
+    constructor(requester, auth) {
+        this.requester = requester;
+        this.auth = auth;
+    }
 
-// POST
-// Receives movie data from moviesController
-// and posts it to the movies collection
+    headers() {
+        return this.auth.getHeaders();
+    }
 
-//  PUT
-// Receives movie data from moviesController
-// and puts it to the movies collection
+    createMovie(data) {
+        return this.requester.post('/movies', data, this.headers());
+    }
 
-// DELETE
-// Receives movie id from moviesController
-// and deletes a movie from movies collection
+    getMovie(id) {
+        return this.requester.get(`/movies/${id}`, this.headers());
+    }
+
+    getMovies() {
+        return this.requester.get('/movies', this.headers());
+    }
+
+    updateMovie(id, data) {
+        return this.requester.put(`/movies/${id}`, data, this.headers())
+    }
+
+    deleteMovie(id) {
+        return this.requester.delete(`/movies/${id}`, this.headers());
+    }
+}
