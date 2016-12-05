42e95198120a05b91d3783123f19a0557ec5f2a2
bosak <bosakmaw@gmail.com>
2016-12-05 22:14:41 +0200

    create Comments model

--- scripts/models/CommentModel.js
+++ scripts/models/CommentModel.js
@@ -1,5 +1,31 @@
 class CommentModel {
-    constructor() {
+    constructor(requester, auth) {
+        this.requester = requester;
+        this.auth = auth;
     }
 
+    get headers() {
+        return this.auth.getHeaders();
+    }
+
+    createComment(movieId, data) {
+        data.movieId = movieId;
+        return this.requester.post('/comments', this.headers, data);
+    }
+
+    getComment(id) {
+        return this.requester.get(`/comments/${id}`, this.headers);
+    }
+
+    getComments(movieId) {
+        return this.requester.get(`/comments?query={"movieId":${movieId}}`, this.headers);
+    }
+
+    updateComment(id, data) {
+        return this.requester.put(`/comments/${id}`, this.headers, data);
+    }
+
+    deleteComment(id) {
+        return this.requester.delete(`/comments/${id}`, this.headers);
+    }
 }
